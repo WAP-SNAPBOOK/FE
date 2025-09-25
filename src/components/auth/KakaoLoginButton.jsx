1681c03ba698b1793fcba7658fc372d49118bd66
jinohpark371 <jinoh1030@naver.com>
2025-09-25 14:59:10 +0900

    refactor: KakaoLoginButton에서 환경변수 제거

--- src/components/auth/KakaoLoginButton.jsx
+++ src/components/auth/KakaoLoginButton.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { kakaoAuthService } from '../../api/auth/kakaoAuthService';
 
-const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
-const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-
 function KakaoLoginButton() {
   const handleLogin = () => {
     window.location.href = kakaoAuthService.getAuthUrl(); //카카오 로그인 URL변경
