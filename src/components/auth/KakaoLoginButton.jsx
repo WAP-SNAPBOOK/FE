91048f9c22c04a5b4602523491333dd6988e1ac9
jinohpark371 <jinoh1030@naver.com>
2025-09-25 16:06:20 +0900

    Design: 카카오버튼 UI 초기 구현

--- src/components/auth/KakaoLoginButton.jsx
+++ src/components/auth/KakaoLoginButton.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { kakaoAuthService } from '../../api/auth/kakaoAuthService';
+import { Button } from '../common/Button';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+import kakaoIcon from '../../assets/icons/kakao-icon.svg';
+
+const KakaoButton = styled(Button).attrs({
+  $align: 'flex-start',
+  $gap: '35px',
+})`
+  background: ${theme.colors.kakakoYellow};
+  color: ${theme.colors.kakaoTextBlack};
+  border-radius: ${theme.radius.md};
+
+  min-width: 220px;
+
+  /* 호버 시 살짝 더 진하게 */
+  &:hover {
+    background: #fdd835;
+  }
+`;
 
 function KakaoLoginButton() {
   const handleLogin = () => {
     window.location.href = kakaoAuthService.getAuthUrl(); //카카오 로그인 URL변경
   };
 
-  return <button onClick={handleLogin}>카카오 로그인</button>;
+  return (
+    <KakaoButton onClick={handleLogin}>
+      <img src={kakaoIcon} alt="kakao symbol" aria-hidden="true" />
+      <span>카카오로 계속하기</span>
+    </KakaoButton>
+  );
 }
 
 export default KakaoLoginButton;
