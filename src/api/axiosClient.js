2938dc1e9754b7a07d814486b50b0b91c33c539d
jinohpark371 <jinoh1030@naver.com>
2025-09-25 21:29:50 +0900

    fix: axios인스턴스 baseURL 수정

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURl: import.meta.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default axiosClient;
