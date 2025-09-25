c4f667c950ed789352b13ed9c4fa64394e8c3728
jinohpark371 <jinoh1030@naver.com>
2025-09-25 14:36:54 +0900

    chore: 초기 App 구성 및 기본 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import KakaoLoginButton from './components/auth/KakaoLoginButton';
+import AuthRedirectPage from './pages/AuthRedirectPage';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <div>hello snapbook</div>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<KakaoLoginButton />} />
+          <Route path="/auth" element={<AuthRedirectPage />} />
+        </Routes>
+      </BrowserRouter>
+
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
