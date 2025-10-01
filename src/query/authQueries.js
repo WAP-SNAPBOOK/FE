6fb2cf6fac9dd8c89f22e31a7f411a204274cab2
jinohpark371 <jinoh1030@naver.com>
2025-10-01 19:46:29 +0900

    feat: 회원가입 필요에 따른 페이지 라우팅 구현

--- src/query/authQueries.js
+++ src/query/authQueries.js
@@ -1,13 +1,24 @@
 import { useMutation } from '@tanstack/react-query';
 import { kakaoAuthService } from '../api/services/kakaoAuthService';
 import { authStorage } from '../utils/authStorage';
+import { useNavigate } from 'react-router-dom';
 
 export const useHandleAuthCode = () => {
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: (code) => kakaoAuthService.exchangeCodeForToken(code),
     onSuccess: (data) => {
-      //로그인 성공 시 응답 데이터를 저장
+      // 로그인 성공 시 응답 데이터를 저장
       authStorage.save(data);
+
+      const status = data?.authStatus;
+      if (status === 'SIGNUP_REQUIRED') {
+        navigate('/signup');
+      } else {
+        // 로그인 성공
+        navigate('/');
+      }
     },
   });
 };
