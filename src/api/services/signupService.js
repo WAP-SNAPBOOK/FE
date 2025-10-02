6ac97663c32f483ecaca82159312ae5813c870aa
jinohpark371 <jinoh1030@naver.com>
2025-10-02 19:23:31 +0900

    fix: 회원가입 요청 시 payload 전달하도록 수정

--- src/api/services/signupService.js
+++ src/api/services/signupService.js
@@ -2,7 +2,7 @@ import axiosClient from '../axiosClient';
 
 export const signupService = {
   signupCustomer: async (payload) => {
-    const res = await axiosClient.post('/user/customer/signup');
+    const res = await axiosClient.post('/user/customer/signup', payload);
     return res.data;
   },
 
