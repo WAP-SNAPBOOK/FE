8685fff3945c1092350effdc6cc6e08f20009fb1
jinohpark371 <jinoh1030@naver.com>
2025-10-05 19:14:23 +0900

    fix: 회원탈퇴, 회원가입 분기 상태 로직 개선

--- src/query/authQueries.js
+++ src/query/authQueries.js
@@ -2,11 +2,11 @@ import { useMutation } from '@tanstack/react-query';
 import { kakaoAuthService } from '../api/services/kakaoAuthService';
 import { authStorage } from '../utils/auth/authStorage';
 import { useNavigate } from 'react-router-dom';
-import { useAUth } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import normalizeAuthResponse from '../utils/auth/normalizeAuthResponse';
 
 export const useHandleAuthCode = () => {
-  const { login } = useAUth();
+  const { login } = useAuth();
   const navigate = useNavigate();
 
   return useMutation({
@@ -14,7 +14,7 @@ export const useHandleAuthCode = () => {
     onSuccess: (data) => {
       const normalized = normalizeAuthResponse(data);
       //토큰은 스토리지에 저장
-      authStorage.saveTokens(normalized);
+      authStorage.saveTokens(normalized.tokens);
 
       //사용자 정보는 Context에 따로 관리
       login(normalized.profile);
@@ -22,7 +22,7 @@ export const useHandleAuthCode = () => {
       //회언가입 분기처리
       if (data.authStatus === 'SIGNUP_REQUIRED') {
         //신규 유저 -> 가입 선택 화면으로
-        navigate('/signup');
+        navigate('/signup', { state: { isSignupRequired: true } });
       } else {
         // 로그인 성공
         navigate('/');
@@ -33,7 +33,7 @@ export const useHandleAuthCode = () => {
 
 // 회원탈퇴
 export function useDeleteUser() {
-  const { logout } = useAUth();
+  const { logout } = useAuth();
   const navigate = useNavigate();
 
   return useMutation({
