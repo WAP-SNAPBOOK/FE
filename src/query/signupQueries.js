17fa6a6d177dc78a8cc752f8988ef6076a23e253
jinohpark371 <jinoh1030@naver.com>
2025-10-06 14:56:41 +0900

    fix: 점주, 고객 회원가입 쿼리 성공 후 로직 개선

--- src/query/signupQueries.js
+++ src/query/signupQueries.js
@@ -2,20 +2,18 @@ import { useMutation } from '@tanstack/react-query';
 import { signupService } from '../api/services/signupService';
 import { authStorage } from '../utils/auth/authStorage';
 import { useNavigate } from 'react-router-dom';
-import normalizeAuthResponse from '../utils/auth/normalizeAuthResponse';
 import { useAuth } from '../context/AuthContext';
 export const useSignupCustomer = () => {
+  const { login } = useAuth();
   const naviagate = useNavigate();
   return useMutation({
     mutationFn: (payload) => signupService.signupCustomer(payload),
     onSuccess: (data) => {
       //고객으로 회원가입 성공시 기존 데이터 다 날리기
       authStorage.clear();
-      //회원가입 정보 정규화
-      const normalized = normalizeAuthResponse(data);
 
-      //응답 구조에 맞춰 사용자 정보 저장
-      authStorage.save(normalized);
+      //사용자 정보 전역 상태 + 스토리지 저장
+      login(data);
 
       //마지막으로 홈화면 이동
       naviagate('/');
@@ -24,7 +22,7 @@ export const useSignupCustomer = () => {
 };
 
 export const useSignupOwner = () => {
-  const setUser = useAuth();
+  const { login } = useAuth();
   const navigate = useNavigate();
   return useMutation({
     mutationFn: (payload) => signupService.signupOwner(payload),
@@ -32,14 +30,8 @@ export const useSignupOwner = () => {
       //점주로 회원가입 성공시 기존 데이터 다 날리기
       authStorage.clear();
 
-      //회원가입 정보 정규화
-      const normalized = normalizeAuthResponse(data);
-
-      //응답 구조에 맞춰 사용자 정보 저장
-      authStorage.saveTokens(normalized.tokens);
-
-      //사용자 정보 전역 상태 추가
-      setUser(normalized.profile);
+      //사용자 정보 전역 상태 + 스토리지 저장
+      login(data);
 
       navigate('/');
     },
