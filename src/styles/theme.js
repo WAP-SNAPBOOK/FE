e160eb57bd547a1f15457cfb1585abdd14e8f8aa
jinohpark371 <jinoh1030@naver.com>
2025-10-08 21:29:03 +0900

    feat: theme 공통 색상 관리 구조 구성

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,6 +2,26 @@ const theme = {
   colors: {
     kakakoYellow: '#FEE500',
     kakaoTextBlack: 'rgba(0,0,0,0.85)',
+
+    // 공통 색상
+    white: '#FFFFFF',
+    black: {
+      DEFAULT: '#000000',
+      20: 'rgba(0 0, 0, 0.2)',
+      75: 'rgba(0 0, 0, 0.75)',
+    },
+    gray: {
+      DEFAULT: '#D1D3D8',
+      20: 'rgba(209, 211, 216, 0.2)',
+      25: 'rgba(209, 211, 216, 0.25)',
+    },
+
+    primary: '#F08080',
+    focus: '#F08080',
+
+    success: '#4CAF50',
+    warning: '#FFC107',
+    error: '#F44336',
   },
   radius: {
     md: '12px',
