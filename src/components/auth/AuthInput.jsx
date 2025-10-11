61ea423c665c9ea529d2dc689d1f8008e7c1f9ce
jinohpark371 <jinoh1030@naver.com>
2025-10-11 17:43:51 +0900

    feat: 회원가입 용 input컴포넌트 구현

---
+++ src/components/auth/AuthInput.jsx
@@ -0,0 +1,26 @@
+// src/components/input/AuthInput.jsx
+import styled, { css } from 'styled-components';
+import { Input } from '../common/input';
+import theme from '../../styles/theme';
+
+export const AuthInput = styled(Input).attrs({
+  $border: `1.5px solid ${theme.colors.gray.border}`,
+  $color: `${theme.colors.black[90]}`,
+  $placeholder: `${theme.colors.black[30]}`,
+  $bg: 'white',
+  $radius: '10px',
+})`
+  &::placeholder {
+    color: ${({ $placeholder }) => $placeholder};
+  }
+
+  &:focus {
+    border-color: ${theme.colors.highlight.DEFAULT};
+  }
+
+  &:disabled {
+    background-color: ${theme.colors.gray[100]};
+    color: ${theme.colors.gray[400]};
+    cursor: not-allowed;
+  }
+`;
