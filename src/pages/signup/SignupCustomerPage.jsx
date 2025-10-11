7e51c302b04c20d1d50c303d724485fa5e221458
jinohpark371 <jinoh1030@naver.com>
2025-10-11 18:20:09 +0900

    design: 고객용 회원가입 페이지 UI 구현

--- src/pages/signup/SignupCustomerPage.jsx
+++ src/pages/signup/SignupCustomerPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { authStorage } from '../../utils/auth/authStorage';
 import { useSignupCustomer } from '../../query/signupQueries';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { SignupTitle } from '../../components/title/SignupTitle';
+import Container from '../../components/common/Container';
+import { AuthInput } from '../../components/auth/AuthInput';
+import { NextButton } from '../../components/common/NextButton';
 
 function SignupCustomerPage() {
   const navigate = useNavigate();
@@ -16,25 +18,33 @@ function SignupCustomerPage() {
     if (!isSignupRequired) navigate('/');
   }, [navigate]);
 
+  const onChange = (e) => setNickname(e.target.value);
+
   const onSubmit = (e) => {
     e.preventDefault();
     signup.mutate({ nickname });
   };
 
   return (
-    <form onSubmit={onSubmit} style={{ padding: 24 }}>
-      <SignupTitle>고객 회원가입</SignupTitle>
-      <label>
-        닉네임
-        <input value={nickname} onChange={(e) => setNickname(e.target.value)} required />
-      </label>
-      <div style={{ marginTop: 12 }}>
-        <button type="submit" disabled={signup.isPending}>
-          {signup.isPending ? '가입 중...' : '가입하기'}
-        </button>
+    <Container>
+      <div className="w-[305px] h-[530px] flex flex-col items-center">
+        <SignupTitle>고객 회원가입</SignupTitle>
+        <form onSubmit={onSubmit} className="w-full flex flex-col items-center">
+          <label key={'닉네임'} className="w-full block mb-[15px]">
+            <AuthInput
+              name={'닉네임'}
+              value={nickname}
+              placeholder={'닉네임'}
+              onChange={onChange}
+            />
+          </label>
+          <NextButton type="submit" disabled={signup.isPending} className="mt-[30px]">
+            {signup.isPending ? '가입중...' : '가입하기'}
+          </NextButton>
+          {signup.isError && <p style={{ color: 'red' }}>가입 실패: {signup.error?.message}</p>}
+        </form>
       </div>
-      {signup.isError && <p style={{ color: 'red' }}>가입 실패: {signup.error?.message}</p>}
-    </form>
+    </Container>
   );
 }
 
