b0334e4722464c43960f8c1a3da00925ca14d51a
jinohpark371 <jinoh1030@naver.com>
2025-10-17 21:07:55 +0900

    chore: PWA용 사진 추가 및 추가 설정

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       manifest: {
         name: 'Snapbook',
         short_name: 'Snapbook',
-        description: '희귀 의류 아카이빙 플랫폼',
+        description: '예약 플랫폼',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
@@ -25,19 +25,19 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/logo-icon.svg',
+            src: '/logo-icon-192x192.png',
             sizes: '192x192',
-            type: 'image/svg+xml',
+            type: 'image/png',
           },
           {
-            src: '/logo-icon.svg',
+            src: '/logo-icon-512x512.png',
             sizes: '512x512',
-            type: 'image/svg+xml',
+            type: 'image/png',
           },
           {
-            src: '/logo-icon.svg',
+            src: '/logo-icon-512x512.png',
             sizes: '512x512',
-            type: 'image/svg+xml',
+            type: 'image/png',
             purpose: 'any maskable',
           },
         ],
