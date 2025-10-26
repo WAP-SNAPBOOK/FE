8a50b3adc46d6544440b1c13d6a0eaee94752d04
jinohpark371 <jinoh1030@naver.com>
2025-10-26 18:14:38 +0900

    feat: 임시 홈화면 회원탈퇴 구현

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { Button } from '../../components/common/Button';
+import { useDeleteUser } from '../../query/authQueries';
 
 export default function HomePage() {
-  const { auth, logout } = useAuth();
+  //사용자 정보 전역 상태
+  const { auth } = useAuth();
+  //회원탈퇴 훅
+  const deleteuser = useDeleteUser();
+
+  //회원탈퇴 헨들러
+  const deleteUserHandler = () => {
+    deleteuser.mutate();
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
@@ -11,7 +20,7 @@ export default function HomePage() {
       {auth ? (
         <>
           <p className="mb-2">안녕하세요, {auth.name || '사용자'}님 </p>
-          <Button onClick={logout}>임시 회원탈퇴</Button>
+          <Button onClick={deleteUserHandler}>임시 회원탈퇴</Button>
         </>
       ) : (
         <p>로그인 정보가 없습니다.</p>
