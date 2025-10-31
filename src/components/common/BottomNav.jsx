8071b83dd76ba6d3f54f92df2eece55f15c5fa9d
jinohpark371 <jinoh1030@naver.com>
2025-10-31 16:22:32 +0900

    feat: nav 컨테이너 구성

---
+++ src/components/common/BottomNav.jsx
@@ -0,0 +1,15 @@
+const NavContainer = styled.nav`
+  position: fixed;
+  bottom: 16px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 132px;
+  height: 53px;
+  background: white;
+  border: 2px solid ${theme.colors.gray.border};
+  border-radius: 30px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.08);
+`;
