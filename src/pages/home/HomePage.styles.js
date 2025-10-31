2fac396ae1051e8da93fb5f0053dbd12b6a3073f
jinohpark371 <jinoh1030@naver.com>
2025-10-31 15:06:03 +0900

    feat: 홈 내용 영역 컴포넌트 구현

--- src/pages/home/HomePage.styles.js
+++ src/pages/home/HomePage.styles.js
@@ -4,3 +4,10 @@ export const ButtonGroup = styled.div`
   display: flex;
   gap: 16px;
 `;
+
+export const CenterArea = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
