b1ebd568690e9f384d7cd5731f2e30955f031b9d
jinohpark371 <jinoh1030@naver.com>
2025-11-02 19:30:49 +0900

    fix: AuthRedirectPage 파일 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthRedirectPage from './pages/AuthRedirectPage';
+import AuthRedirectPage from './pages/redirect/AuthRedirectPage';
 import SignupGatePage from './pages/signup/SignupGatePage';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from './context/AuthContext';
