b3fa0ef8127c9cb936c20297e07ccee67fc2f976
jinohpark371 <jinoh1030@naver.com>
2025-11-04 17:00:09 +0900

    fix: slug가진 회원 채팅방 리다이렉트 페이지 이동 구조로 수정

--- src/query/authQueries.js
+++ src/query/authQueries.js
@@ -22,8 +22,13 @@ export const useHandleAuthCode = () => {
         //신규 유저 -> 가입 선택 화면으로(링크 접속 사용자는 식별코드 가지고)
         navigate(`/signup?slug=${slug || ''}`, { state: { isSignupRequired: true } });
       } else {
-        // 로그인 성공
-        navigate('/');
+        //식별 코드 있다면 바로 채팅방 리다이렉트용 임시페이지 이동
+        if (slug) {
+          navigate(`/s/${slug}`, { replace: true });
+        } else {
+          // 일반 로그인시 홈으로
+          navigate('/');
+        }
       }
     },
     onError: (error) => {
