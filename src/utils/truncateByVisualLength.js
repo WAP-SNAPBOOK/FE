71f33b60a864e16dced0904b0fd5cd9f771aa3c4
jinohpark371 <jinoh1030@naver.com>
2025-11-07 20:34:53 +0900

    feat: 채팅 마지막 메시지 길이 제어 함수 구현

---
+++ src/utils/truncateByVisualLength.js
@@ -0,0 +1,20 @@
+//채팅 마지막 메시지 길이 제어 함수
+export function truncateByVisualLength(str, maxLength) {
+  let visualLen = 0; // 화면상 누적 폭
+  let result = ''; // 잘라낸 문자열 결과
+
+  for (const ch of str) {
+    // 한글, 한자, 일본어 폭 2로 계산, 나머지 폭 1로 계산
+    visualLen += /[ㄱ-ㅎ가-힣\u3131-\uD79D\u4E00-\u9FFF]/.test(ch) ? 2 : 1;
+
+    //폭 기준으로 초과한 시점
+    if (visualLen > maxLength) {
+      result += '…';
+      break;
+    }
+
+    result += ch;
+  }
+
+  return result;
+}
