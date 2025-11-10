cc63c8fd4d6bb328812a304f928caac668fbd39c
jinohpark371 <jinoh1030@naver.com>
2025-11-10 17:40:05 +0900

    fix: 낙관적 업데이트 훅, 메시지 관련 세분화 컴퍼넌트 적용

--- src/pages/chat/ChatRoomPage.jsx
+++ src/pages/chat/ChatRoomPage.jsx
@@ -2,13 +2,8 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 import { useSendMessage, useChatMessages } from '../../query/chatQueries';
 import { chatSocketService } from '../../api/services/chatSocketService';
-import DateDivider from '../../components/chat/DateDivider';
-import dayjs from 'dayjs';
-import { formatTime } from '../../utils/formatTime';
 import Container from '../../components/common/Container';
 import * as S from './ChatRoomPage.style';
-import sendIcon from '../../assets/icons/send-icon.svg';
-import addIcon from '../../assets/icons/add-icon.svg';
 import backIcon from '../../assets/icons/back-icon.svg';
 import { ChatRoomTitle } from '../../components/title/SignupTitle';
 import ReservationModal from '../../components/reservation/ReservationModal';
@@ -18,10 +13,12 @@ import { usePreserveScrollPosition } from '../../hooks/chat/usePreserveScrollPos
 import { useInitScrollToBottom } from '../../hooks/chat/useInitScrollToBottom';
 import { useTopObserver } from '../../hooks/chat/useTopObserver';
 import { useShopInfoByCode } from '../../query/linkQueries';
-import { ReservationCompleteMessage } from '../../components/message/ReservationCompleteMessage';
 import AddMenuButton from '../../components/chat/AddMenuButton';
 import ChatMenuPanel from '../../components/chat/ChatMenuPanel';
 import ChatSumbitButton from '../../components/chat/chatSumbitButton';
+import MessageList from '../../components/message/MessageList';
+import { useOptimisticMessage } from '../../hooks/chat/useOptimisticMessage';
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function ChatRoomPage() {
   const [input, setInput] = useState('');
@@ -36,10 +33,28 @@ export default function ChatRoomPage() {
   //메뉴 표시 여부 상태
   const [showMenu, setShowMenu] = useState(false);
 
+  //전역 상태 사용자 ID
+  const { auth } = useAuth();
+  const userId = auth?.userId;
+
+  const accessToken = authStorage.getAccessToken();
+
+  //해당 채팅방 ID
+  const { chatRoomId } = useParams();
+
+  const queryClient = useQueryClient();
+
   //링크 유입시 가게 정보 조회
   const [searchParams] = useSearchParams();
   const slugOrCode = searchParams.get('slug');
   const { data: shopInfo } = useShopInfoByCode(slugOrCode);
+
+  //메세지 전송 관련 훅
+  const { addOptimisticMessage, replaceWithServerMessage } = useOptimisticMessage(
+    setLiveMessages,
+    userId,
+    chatRoomId
+  );
 
   const handleBack = () => {
     if (window.history.state && window.history.state.idx > 0) {
@@ -57,9 +72,6 @@ export default function ChatRoomPage() {
     }, 150);
   };
 
-  const { auth } = useAuth();
-  const userId = auth?.userId;
-
   //스크롤 위치 제어용 ref
   const messageListRef = useRef(null);
   //스크롤 감지용 ref
@@ -68,10 +80,6 @@ export default function ChatRoomPage() {
   const bottomRef = useRef(null);
 
   const navigate = useNavigate();
-  //해당 채팅방 ID
-  const { chatRoomId } = useParams();
-
-  const accessToken = authStorage.getAccessToken();
 
   // 기존 메시지, cursor (HTTP GET 기반)
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isSuccess } =
@@ -81,58 +89,31 @@ export default function ChatRoomPage() {
   const oldMessages = data?.pages.flatMap((page) => page.messages).reverse() ?? [];
   //메시지 전송 훅
   const { mutate: sendMessage } = useSendMessage(chatRoomId, (message) => {
-    // 낙관적 업데이트(즉시 메시지 표시)
-    const now = new Date();
-    const tempId = `temp-${Date.now()}`;
-
-    // 서버와 동일한 구조로 임시 메시지 반영
-    setLiveMessages((prev) => [
-      ...prev,
-      {
-        clientId: tempId,
-        messageId: Date.now(), // 임시 ID
-        senderId: userId,
-        senderName: 'me', // TODO: 백앤드에서 보낸 실제 사용자 본인 이름으로 교체
-        message,
-        sentAt: now.toISOString(),
-        pending: true,
-        roomId: Number(chatRoomId),
-      },
-    ]);
+    //메시지 낙관적 업데이트
+    addOptimisticMessage(message);
   });
+
+  // React Query 캐시 초기화
+  useEffect(() => {
+    return () => {
+      queryClient.removeQueries(['messages', chatRoomId]);
+    };
+  }, [chatRoomId]);
 
   //WebSocket 연결
   useEffect(() => {
     chatSocketService.connect(accessToken, () => {
       //구독시 상대방 메시지 실시간 업데이트
       chatSocketService.subscribe(chatRoomId, (incoming) => {
-        setLiveMessages((prev) => {
-          // 1. 같은 메시지(내가 낙관적으로 추가한 메시지)인지 확인
-          const idx = prev.findIndex(
-            (m) =>
-              m.pending &&
-              m.clientId &&
-              m.message === incoming.message &&
-              Math.abs(new Date(m.sentAt) - new Date(incoming.sentAt)) < 2000
-          );
-
-          if (idx >= 0) {
-            // 2. 낙관적 메시지를 서버 메시지로 교체
-            const updated = [...prev];
-            updated[idx] = { ...incoming, pending: false };
-            return updated;
-          }
-
-          // 3. 새로운 메시지면 그냥 추가
-          return [...prev, incoming];
-        });
+        // 낙관적 메시지를 서버 메시지로 교체
+        replaceWithServerMessage(incoming);
       });
     });
 
     return () => {
       chatSocketService.disconnect();
     };
-  }, [chatRoomId]);
+  }, [chatRoomId, replaceWithServerMessage]);
 
   //스크롤 제어(새로운 메시지 추가시 추가된 매시지 보기)
   // 메시지 전송 후 호출
@@ -207,58 +188,13 @@ export default function ChatRoomPage() {
           </ChatRoomTitle>
           <S.BookButton onClick={openModal}>예약</S.BookButton>
         </S.Header>
-        <S.MessageList ref={messageListRef}>
+        <S.Messages ref={messageListRef}>
           {/*상단 스크롤 감지용 */}
           <div ref={topObserverRef} />
-          {allMessages.map((msg, index) => {
-            const isMine = msg.senderId === userId; //사용자 본인 Id를 통한 메시지 판별
-            const isReservationCard = msg.isReservationCard; // 예약 전송 완료 메시지 여부
-
-            const currentDate = dayjs(msg.sentAt).format('YYYY-MM-DD'); //현재 날짜
-            const prevDate =
-              index > 0 ? dayjs(allMessages[index - 1].sentAt).format('YYYY-MM-DD') : null; //이전 날짜, 메시지 하나일 시 null
-
-            const showDateDivider = currentDate !== prevDate; //다른 날짜 판별 기준값
-
-            return (
-              <React.Fragment key={`${index}-${msg.messageId}`}>
-                {/*이전 날짜와 현재 날짜과 다르다면 구분선 추가*/}
-                {showDateDivider && <DateDivider date={msg.sentAt} />}
-                {/*예약 완료 메시지*/}
-                {isReservationCard ? (
-                  <S.MessageRow $isMine={false}>
-                    <S.Bubble $isMine={false}>
-                      <ReservationCompleteMessage
-                        name={msg.payload.name}
-                        date={msg.payload.date}
-                        time={msg.payload.time}
-                        photoCount={msg.payload.photoCount}
-                      />
-                    </S.Bubble>
-                  </S.MessageRow>
-                ) : (
-                  //일반 메시지 렌더링, 상대방, 본인 메시지에 따른 정렬
-
-                  <S.MessageRow $isMine={isMine}>
-                    {isMine ? (
-                      <>
-                        <S.Time>{formatTime(msg.sentAt)}</S.Time>
-                        <S.Bubble $isMine>{msg.message}</S.Bubble>
-                      </>
-                    ) : (
-                      <>
-                        <S.Bubble $isMine={false}>{msg.message}</S.Bubble>
-                        <S.Time>{formatTime(msg.sentAt)}</S.Time>
-                      </>
-                    )}
-                  </S.MessageRow>
-                )}
-              </React.Fragment>
-            );
-          })}
+          <MessageList messages={allMessages} userId={userId} />
           {/* 하단 스크롤 고정용 */}
           <div ref={bottomRef} />
-        </S.MessageList>
+        </S.Messages>
         {/* 채팅 메뉴 패널 */}
         <ChatMenuPanel visible={showMenu} />
 
