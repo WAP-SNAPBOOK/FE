897a9a7365e5f07d8de37cdb1e2009784f015d22
jinohpark371 <jinoh1030@naver.com>
2025-11-17 20:14:31 +0900

    fix: className 변경에 따른 수정

--- src/components/message/ReservationCompleteMessage.jsx
+++ src/components/message/ReservationCompleteMessage.jsx
@@ -16,19 +16,19 @@ export default function ReservationCompleteMessage({ name, date, time, photoCoun
       {/* 예약 정보 */}
       <div className="reservation-info">
         <div className="info-row">
-          <span className="label">고객명</span>
+          <span className="label-com">고객명</span>
           <span className="value name">{name}</span>
         </div>
         <div className="info-row">
-          <span className="label">예약 날짜</span>
+          <span className="label-com">예약 날짜</span>
           <span className="value date">{date}</span>
         </div>
         <div className="info-row">
-          <span className="label">예약 시간</span>
+          <span className="label-com">예약 시간</span>
           <span className="value time">{time}</span>
         </div>
         <div className="info-row">
-          <span className="label">첨부 사진</span>
+          <span className="label-com">첨부 사진</span>
           <span className="value photo-count">{photoCount ?? 0}장</span>
         </div>
       </div>
