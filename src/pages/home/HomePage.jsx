4ea8b937e59bea1d2d18a8c76578963acf9cf6e9
jinohpark371 <jinoh1030@naver.com>
2025-11-25 18:46:46 +0900

    Revert "fix: 홈페이지 임시 회원 탈퇴 기능 삭제"
    This reverts commit e36df53c86548ea00c5607fb22fb603ce62bb753.

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import { useDeleteUser } from '../../query/authQueries';
 import * as S from './HomePage.styles';
 import ChatIcon from '../../assets/icons/mainChat-icon.svg';
 import BookIcon from '../../assets/icons/book-icon.svg';
@@ -6,8 +9,20 @@ import Container from '../../components/common/Container';
 import Header from '../../components/common/Header';
 import MainActionButton from '../../components/home/MainActionButton ';
 import BottomNav from '../../components/common/BottomNav';
+import { BaseButton } from '../../components/common/Button';
+import { NextButton } from '../../components/common/NextButton';
+
 export default function HomePage() {
   const navigate = useNavigate();
+  //사용자 정보 전역 상태
+  const { auth } = useAuth();
+  //회원탈퇴 훅
+  const deleteuser = useDeleteUser();
+
+  //회원탈퇴 헨들러
+  const deleteUserHandler = () => {
+    deleteuser.mutate();
+  };
 
   //채팅방 목록 이동
   const goToChat = () => {
@@ -16,7 +31,7 @@ export default function HomePage() {
 
   // 예약 내역 이동
   const goToReservationList = () => {
-    navigate('/reservations');
+    navigate('/reservations'); // or 실제 라우트 이름에 맞게 수정
   };
 
   return (
@@ -27,6 +42,10 @@ export default function HomePage() {
           <MainActionButton onClick={goToChat} icon={ChatIcon} label="채팅방 조회" />
           <MainActionButton onClick={goToReservationList} icon={BookIcon} label="예약 내역" />
         </S.ButtonGroup>
+        {/* 임시 회원탈퇴 버튼 */}
+        <div className="fixed bottom-24 left-1/2 -translate-x-1/2 w-[90%] flex justify-center">
+          <NextButton onClick={deleteUserHandler}>임시 회원 탈퇴</NextButton>
+        </div>
         <BottomNav />
       </S.CenterArea>
     </Container>
