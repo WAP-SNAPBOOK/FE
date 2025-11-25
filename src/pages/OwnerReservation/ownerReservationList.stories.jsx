0a1b1034eba99cc62461743bf8403ce224b00148
juya0 <minju6669@icloud.com>
2025-11-26 01:54:04 +0900

    feat: 사장님 예약 내역 목록 수정
    색 변경, 주석 추가

--- src/pages/OwnerReservation/ownerReservationList.stories.jsx
+++ src/pages/OwnerReservation/ownerReservationList.stories.jsx
@@ -1,11 +1,19 @@
-import OwnerReservationList from './ownerReservationList';
+import React from 'react';
+import OwnerReservationList from './ownerReservationList.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const meta = {
+const queryClient = new QueryClient();
+
+export default {
+  title: 'Pages/OwnerReservationList',
   component: OwnerReservationList,
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
 };
 
-export default meta;
-
-export const Default = {
-  args: {},
-};
+export const Default = {};
