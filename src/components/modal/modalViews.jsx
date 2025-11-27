1bd48c6c6da9c1162d425733ed13235944f289c2
jinohpark371 <jinoh1030@naver.com>
2025-11-27 15:04:20 +0900

    fix: 안드로이드 관련 PWA 안내사항 수정
    버튼으로 크롭으로 이동 기능 삭제, 삼성 인터넷 브라우저 기준 안내 문구 추가 및 사진 적용

--- src/components/modal/modalViews.jsx
+++ src/components/modal/modalViews.jsx
@@ -1,22 +1,23 @@
-import androidGuideImg_1 from '../../assets/info/android_chrome_pwa_1.png';
-import androidGuideImg_2 from '../../assets/info/android_chrome_pwa_2.png';
+import androidGuideImg_1 from '../../assets/info/android_pwa_1.png';
+import androidGuideImg_2 from '../../assets/info/android_pwa_2.png';
+import androidGuideImg_3 from '../../assets/info/android_pwa_3.png';
 import iosGuideImg_1 from '../../assets/info/ios_safari_pwa_1.png';
 import iosGuideImg_2 from '../../assets/info/ios_safari_pwa_2.png';
 
-export const modalViews = (setShowModal, openIntent) => ({
+export const modalViews = (setShowModal) => ({
   android: {
     title: 'Android 안내',
     content: (
       <>
         인앱에서는 설치가 불가능합니다.
         <br />
-        아래 안내를 참고해 Chrome에서 열어주세요.
+        아래 안내를 참고해 Samsung Internet 브라우저에서 열어주세요.
         <br />
-        1. 크롬 오른쪽 위 ⋮ 메뉴를 누르세요.
+        1. 브라우저 오른쪽 위 ⋮ 메뉴를 누르세요.
         <br />
-        2. [홈 화면에 추가]를 선택하세요.
+        2. 상단에 보이는 화살표 아이콘을 누르세요
         <br />
-        3. SNAPBOOK 아이콘이 홈 화면에 추가됩니다.
+        3. 추가버튼을 누르면 SNAPBOOK 아이콘이 홈 화면에 추가됩니다.
         <br />
         <img
           src={androidGuideImg_1}
@@ -28,9 +29,14 @@ export const modalViews = (setShowModal, openIntent) => ({
           alt="안드로이드 브라우저 메뉴 안내"
           style={{ width: '100%', borderRadius: '12px', marginTop: '12px' }}
         />
+        <img
+          src={androidGuideImg_3}
+          alt="안드로이드 브라우저 메뉴 안내"
+          style={{ width: '100%', borderRadius: '12px', marginTop: '12px' }}
+        />
       </>
     ),
-    buttons: [{ label: '크롬으로 열기', onClick: () => openIntent() }],
+    buttons: [{ label: '확인', onClick: () => setShowModal(false) }],
   },
 
   ios: {
