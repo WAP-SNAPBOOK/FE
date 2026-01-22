f1acd539585bd10c98e94016859b39fff57e97ed
jinohpark371 <jinoh1030@naver.com>
2026-01-22 23:32:19 +0900

    chore: @ alias 추가

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -50,5 +51,10 @@ export default defineConfig({
   ],
   define: {
     global: 'window',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
