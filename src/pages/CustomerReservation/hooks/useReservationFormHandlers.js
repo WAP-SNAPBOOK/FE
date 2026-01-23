0cca8a5a3ccdfac878596b7af170a2ea84163895
jinohpark371 <jinoh1030@naver.com>
2026-01-23 15:47:53 +0900

    feat: 예약 각 단계 처리 헨들러 구성

---
+++ src/pages/CustomerReservation/hooks/useReservationFormHandlers.js
@@ -0,0 +1,55 @@
+import { useCallback } from 'react';
+
+export function useReservationFormHandlers(setFormData, setCanNext) {
+  //고객 기본정보 입력 헨들러
+  const handleUserInfoChange = useCallback(
+    ({ name, phoneNumber, isValid }) => {
+      setFormData((p) => ({
+        ...p,
+        basic: { ...p.basic, name, phoneNumber },
+      }));
+      setCanNext(isValid); //다음 단계 진행 여부 판별
+    },
+    [setFormData, setCanNext]
+  );
+
+  //예약 시간 입력 헨들러
+  const handleDateTimeChange = useCallback(
+    ({ date, time, isValid }) => {
+      setFormData((p) => ({
+        ...p,
+        basic: { ...p.basic, date, time },
+      }));
+      setCanNext(isValid);
+    },
+    [setFormData, setCanNext]
+  );
+
+  //사진, 요구사항 입력 헨들러
+  const handlePhotoNoteChange = useCallback(
+    ({ files, notes, isValid }) => {
+      setFormData((p) => ({
+        ...p,
+        photoNote: { files, notes },
+      }));
+      setCanNext(isValid);
+    },
+    [setFormData, setCanNext]
+  );
+
+  //옵션 선택 헨들러
+  const handleOptionsChange = useCallback(
+    ({ options, isValid }) => {
+      setFormData((p) => ({ ...p, options }));
+      setCanNext(isValid);
+    },
+    [setFormData, setCanNext]
+  );
+
+  return {
+    handleUserInfoChange,
+    handleDateTimeChange,
+    handlePhotoNoteChange,
+    handleOptionsChange,
+  };
+}
